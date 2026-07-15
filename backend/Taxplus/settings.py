8f8584954134fbfef5c25ad36706e65f0de4ec7b
deepaksoni <deepaksonise@gmail.com>
2026-07-15 09:26:52 +0530

    Working to complete

--- backend/Taxplus/settings.py
+++ backend/Taxplus/settings.py
@@ -99,7 +99,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         # 'DIRS': [os.path.join(BASE_DIR, "templates")],
-        'DIRS': [os.path.join(BASE_DIR, '../frontend/dist')],
+        'DIRS': [os.path.join(BASE_DIR, '../frontend/dist', os.path.join(BASE_DIR, "templates"))],
 
         'APP_DIRS': True,
         'OPTIONS': {
